48e8ed20dc91b899e84670ed9c2dd4c1d2bf067d
4hiziri <meirvg@gmail.com>
2017-07-14 09:42:44 +0900

    implement function to access api

--- rocket-chat.el
+++ rocket-chat.el
@@ -11,6 +11,20 @@
   (user-id nil)
   (token nil))
 
+(defstruct reg-info
+  email
+  name ; display name
+  password
+  username
+  (active t)
+  (roles (list "user"))
+  (joinDefaultChannels t)
+  (requirePasswordChange nil)
+  (sendWelcomeEmail nil)
+  (verified nil)
+  (customFields "undefined") ;; {field : value}
+  )
+
 (defvar test-url "https://rc.net.itc.nagoya-u.ac.jp")
 (defvar auth-info nil)
 
@@ -32,6 +46,40 @@
     (lambda (&key data &allow-other-keys)
       ,@form)))
 
+(defun bool-to-str (bool)
+  (if bool
+      "true"
+    "false"))
+
+(defun post-json (url header arg-json-alist)
+  (let ((ret nil))
+    (request url
+	     :type "POST"
+	     :data (json-encode-alist arg-json-alist)
+	     :parser 'json-read
+	     :headers (cons '("Content-type" . "application/json") header)
+	     :success (exec-form (setq ret data))
+	     :sync t)
+    ret))
+
+(defun reg-info-to-alist (reg-info)
+  (let ((ret nil))
+    (push `(:email ,(reg-info-email reg-info)) ret)
+    (push `(:name ,(reg-info-name reg-info)) ret)
+    (push `(:password ,(reg-info-password reg-info)) ret)
+    (push `(:username ,(reg-info-username reg-info)) ret)
+    (push `(:active ,(bool-to-str (reg-info-active reg-info))) ret)
+    (push `(:roles ,(list-to-str (reg-info-roles reg-info))) ret)
+    (push `(:joinDefaultChannels ,(bool-to-str (reg-info-joinDefaultChannels reg-info))) ret)
+    (push `(:requirePasswordChange
+	    ,(bool-to-str (reg-info-requirePasswordChange reg-info)))
+	  ret)
+    (push `(:sendWelcomeEmail ,(bool-to-str (reg-info-sendWelcomeEmail reg-info))) ret)
+    (push `(:verified ,(bool-to-str (reg-info-verified reg-info))) ret)
+    (push `(:customFields ,(reg-info-customFields reg-info)) ret)
+    ret))
+
+;; api
 (defun info (url)
   "URL - server url.
 This function return server-info"
@@ -73,9 +121,8 @@ PASSWORD - user's password"
 	  (assoc-val 'message data))
       nil)))
 
-(setf test (login test-url "tkgsy" "se1yao4io"))
-
-(defun me (url auth-token)  
+(defun me (url auth-token)
+  "Get user-info from URL, need AUTH-TOKEN."
   (let ((ret nil))
     (request (url-concat url "/api/v1/me")
 	     :parser 'json-read
@@ -86,6 +133,70 @@ PASSWORD - user's password"
 
 (me test-url test)
 
+;; need test
+;; launch server
+(defun users-create (url auth-token register-info)
+  "Register to URL with AUTH-TOKEN, info is in REGISTER-INFO."
+  (let ((ret nil))
+    (request (concat url "/api/v1/users.create")
+	   :type "POST"
+	   :parser 'json-read
+	   :headers (cons '("Content-type" . "application/json")
+			  (auth-headers auth-token))
+	   :data (json-encode-alist (reg-info-to-alist register-info)) 
+	   :success (exec-form (setq ret data))
+	   :sync t)
+    ret))
+
+(defun users-create-token (url auth-token user-id &optional userid-p)
+  (let ((ret (post-json (concat url "/api/v1/users.createToken")
+			(auth-headers auth-token)
+			(if userid-p
+			    `(("userId" . ,user-id))
+			  `(("username" . ,user-id))))))
+    (if (assoc-val 'success ret)	
+	(let ((data (assoc-val 'data ret)))
+	  (make-auth-token :user-id (assoc-val 'userId data) :token (assoc-val 'authToken data))))))
+
+(defun users-delete (url auth-token)
+  (let ((ret (post-json (concat url "/api/v1/users.delete")
+			(auth-headers auth-token)
+			`(("userId" . ,(auth-token-user-id auth-token))))))
+    ret))
+
+;; :TODO parser
+(defun users-get-avatar (url user-id &optional userid-p)
+  (let ((ret (request (concat url "/api/v1/users.getAvatar")
+		      :params (if userid-p
+				  `(("userId" . ,user-id))
+				`(("username" . ,user-id)))
+		      :success (exec-form)
+		      :sync t)))
+    ret))
+
+(defun users-get-presence (url auth-token user-id &optional userid-p)
+  (let ((ret nil))
+    (request (concat url "/api/v1/users.getPresence")
+	     :params (if userid-p
+			 `(("userId" . ,user-id))
+		       `(("username" . ,user-id)))
+	     :parser 'json-read
+	     :headers (auth-headers auth-token)
+	     :success (exec-form (setq ret data))
+	     :sync t)
+    ret))
+
+(defun users-info (url auth-token user-id &optional userid-p)
+  (let ((ret nil))
+    (request (concat url "/api/v1/users.info")
+	     :params (if userid-p
+			 `(("userId" . ,user-id))
+		       `(("username" . ,user-id)))
+	     :parser 'json-read
+	     :headers (auth-headers auth-token)
+	     :success (exec-form (setq ret data))
+	     :sync t)
+    ret))
 
 (provide 'rocket-chat)
 ;;; rocket-chat ends here
